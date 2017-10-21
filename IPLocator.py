a0565579ae0cf1147301bc0b0bfe96bf9f940bd0
root <root@localhost.localdomain>
2017-10-20 23:06:43 -0700

    added comments

--- IPLocator.py
+++ IPLocator.py
@@ -5,13 +5,13 @@ from colorama import Fore, init
 
 
 def locate(ip, pp):
-    init(autoreset=True)
+    init(autoreset=True)  # initiate colorama, automatically reset color
 
-    r = requests.get("http://ip-api.com/json/{}".format(ip))
+    r = requests.get("http://ip-api.com/json/{}".format(ip))  # get json data about the IP
 
-    json = r.json()
+    json = r.json()  # store json data in "json" variable
 
-    if json.get("status") == "success":
+    if json.get("status") == "success":  # display the data if the request was successful
         keys = ["query", "country", "countryCode", "region", "regionName", "city",
                 "zip", "timezone", "lat", "lon", "org", "isp", "as"]
 
@@ -19,15 +19,15 @@ def locate(ip, pp):
             print("{}{}: {}".format(Fore.LIGHTGREEN_EX, keys[i].upper(),
                                     json.get(keys[i])))
 
-        if pp:
+        if pp:  # if the pinpoint parameter was supplied, open google maps at the latitude and longitude of the IP
             webbrowser.open("https://www.google.com/maps/search/?api=1&query={},{}".format(
                 json.get(keys[8]), json.get(keys[9])), autoraise=True)
     else:
-        print("{}IP location failed! Maybe the address is invalid or down?".format(Fore.LIGHTRED_EX))
+        print("{}IP location failed! Maybe the address is invalid or down?".format(Fore.LIGHTRED_EX))  # display if request was unsuccessful
 
 
-def main():
-    init()
+def main():  # parse arguments
+    init()  # initiate colorama without automatically resetting the color
 
     iplocator = """{} \t[+++] IPLocator v1.1 by kysplease [+++]
   _____ _____  _                     _             
